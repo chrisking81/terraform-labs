999955274bdafc4bc808669297ab144a790f3d2a
Chris King <chris@FLL-DKORN-01.redmond.corp.microsoft.com>
2019-04-17 12:50:18 +0100

    End of lab 3

--- coreNetwork.tf
+++ coreNetwork.tf
@@ -13,24 +13,56 @@ resource "azurerm_public_ip" "vpnGatewayPublicIp" {
 }
 
 resource "azurerm_virtual_network" "core" {
+  name                = "core"
   location            = "${azurerm_resource_group.core.location}"
   resource_group_name = "${azurerm_resource_group.core.name}"
   tags                = "${azurerm_resource_group.core.tags}"
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["1.1.1.1", "1.0.0.1"]
-
-  subnet {
-    name           = "GatewaySubnet"
-    address_prefix = "10.0.0.0/24"
-  }
-
-  subnet {
-    name           = "TrainingSubnet"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "DevSubnet"
-    address_prefix = "10.0.2.0/24"
-  }
 }
+
+resource "azurerm_subnet" "GatewaySubnet" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = "${azurerm_resource_group.core.name}"
+  virtual_network_name = "${azurerm_virtual_network.core.name}"
+  address_prefix       = "10.0.0.0/24"
+}
+
+resource "azurerm_subnet" "TrainingSubnet" {
+  name                 = "TrainingSubnet"
+  resource_group_name  = "${azurerm_resource_group.core.name}"
+  virtual_network_name = "${azurerm_virtual_network.core.name}"
+  address_prefix       = "10.0.1.0/24"
+}
+
+resource "azurerm_subnet" "DevSubnet" {
+  name                 = "DevSubnet"
+  resource_group_name  = "${azurerm_resource_group.core.name}"
+  virtual_network_name = "${azurerm_virtual_network.core.name}"
+  address_prefix       = "10.0.2.0/24"
+}
+
+# resource "azurerm_virtual_network_gateway" "vpnGateway" {
+#   name                = "vpnGateway"
+#   location            = "${azurerm_resource_group.core.location}"
+#   resource_group_name = "${azurerm_resource_group.core.name}"
+#   tags                = "${azurerm_resource_group.core.tags}"
+
+
+#   type     = "Vpn"
+#   vpn_type = "RouteBased"
+
+
+#   active_active = false
+#   enable_bgp    = true
+#   sku           = "Basic"
+
+
+#   ip_configuration {
+#     name                          = "vpnGwConfig1"
+#     public_ip_address_id          = "${azurerm_public_ip.vpnGatewayPublicIp.id}"
+#     private_ip_address_allocation = "Dynamic"
+#     subnet_id                     = "${azurerm_subnet.GatewaySubnet.id}"
+#   }
+# }
+
